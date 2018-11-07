ed8441e7c5cf5a64e473752ab2c5c47870f59aa4
Rafael de Mello Medeiros <rmedeiros@studiovisual.com.br>
2018-11-07 15:27:02 -0200

    Altera método get

--- src/HttpRequest.php
+++ src/HttpRequest.php
@@ -10,45 +10,42 @@ abstract class HttpRequest
 
     protected $headers = [];
 
-    public function post($method, $body = []) {
+    public function post($method, $body = [])
+    {
 
         $args = [
             'timeout' => 45,
             'headers' => $this->headers,
-            'body'    => json_encode( array_merge($this->body, $body) ),
+            'body' => json_encode(array_merge($this->body, $body)),
         ];
 
-        return $this->response(wp_remote_post($this->base_url.$method, $args));
+        return $this->response(wp_remote_post($this->base_url . $method, $args));
     }
 
-     public function get( $params = [] ) {
-
-        $headers = array_merge([
-            'Content-Type' => 'application/json',
-            'Accept' => 'application/json'],
-            $this->headers
-        );
-
+    public function get($method)
+    {
         $args = [
-            'headers' => $headers,
+            'headers' => $this->headers,
             'timeout' => 45,
         ];
 
-        return wp_remote_get(add_query_arg( $params, esc_url_raw( $this->url ) ), $args);
+        return $this->response(wp_remote_get($this->base_url . $method, $args));
     }
 
-    public function delete() {
+    public function delete()
+    {
         $args = [
-            'method'  => 'DELETE',
+            'method' => 'DELETE',
             'timeout' => 45,
             'headers' => $this->headers,
-            'body'    => json_encode($this->body),
+            'body' => json_encode($this->body),
         ];
 
         return wp_remote_request($this->url, $args);
     }
 
-    public function postFile($url, $params = [], $headers = [], $file = []) {
+    public function postFile($url, $params = [], $headers = [], $file = [])
+    {
 
         // initialise the curl request
         $request = curl_init($url);
@@ -85,7 +82,8 @@ abstract class HttpRequest
         ];
     }
 
-    public function put($url, $body = [], $headers = []) {
+    public function put($url, $body = [], $headers = [])
+    {
 
         $headers = array_merge([
             'Content-Type' => 'application/json',
@@ -93,29 +91,29 @@ abstract class HttpRequest
         ], $headers);
 
         $args = [
-            'method'  => 'PUT',
+            'method' => 'PUT',
             'timeout' => 45,
             'headers' => $headers,
-            'body'    => json_encode($body),
+            'body' => json_encode($body),
         ];
 
         $response = wp_remote_request($url, $args);
         $code = wp_remote_retrieve_response_code($response);
         $success = [200, 201];
 
-        if ( ! in_array( $code, $success ) ) {
-            throw new \Exception($code . ' - ' . \wp_remote_retrieve_response_message( $response ) );
+        if (!in_array($code, $success)) {
+            throw new \Exception($code . ' - ' . \wp_remote_retrieve_response_message($response));
         }
 
         return [
-            'body'    => json_decode( wp_remote_retrieve_body( $response ), true ),
-            'headers' => wp_remote_retrieve_headers( $response )
+            'body' => json_decode(wp_remote_retrieve_body($response), true),
+            'headers' => wp_remote_retrieve_headers($response)
         ];
     }
 
     public function response($response)
     {
-        if ( is_wp_error( $response ) ) {
+        if (is_wp_error($response)) {
             return $response->get_error_message();
         } else {
             return isset($response['body']) ? json_decode($response['body'], true) : null;
